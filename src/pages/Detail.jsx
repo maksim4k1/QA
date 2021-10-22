484061a7efdea41400861a641c7da36597ffd7c5
Maksim Bazhenov <maksim4k1@gmail.com>
2021-10-22 14:43:53 +0600

    initial

---
+++ src/pages/Detail.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { useParams } from "react-router";
+
+function Detail () {
+  const {id} = useParams();
+  
+  return(
+    <div>
+      
+    </div>
+  );
+}
+
+export default Detail;
---
+++ src/redux/index.js
@@ -0,0 +1,12 @@
+import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./reducers/rootReducer";
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(
+    applyMiddleware()
+  )
+);
+
+export default store;
