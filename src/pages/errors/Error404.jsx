a867051510da94ce5cf9069fa6d3e31dba97343f
Maksim Bazhenov <maksim4k1@gmail.com>
2021-10-24 00:21:29 +0600

    added styles for 404 page

--- src/pages/errors/Error404.jsx
+++ src/pages/errors/Error404.jsx
@@ -1,10 +1,28 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  margin: 0 0 20px;
+  font-size: 52px;
+  font-weight: 600;
+`;
+const Content = styled.p`
+  font-size: 20px;
+`;
+const Link = styled(NavLink)`
+  color: var(--color-blue);
+  &:hover{
+    text-decoration: underline;
+  }
+`;
 
 function Error404 () {
   return(
-    <div>
-      
-    </div>
+    <main className="container content">
+      <Title>Error: 404</Title>
+      <Content>Page not found. <Link to="/">Go to Main page</Link></Content>
+    </main>
   );
 }
 
